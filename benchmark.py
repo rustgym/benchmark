916321551d7354da276e4c6aad3267b16af5d461
YINCHU XIA <larry.fantasy@gmail.com>
2023-05-09 14:57:21 -0700

    fix env

--- benchmark.py
+++ benchmark.py
@@ -3,22 +3,23 @@ import benchmark_pb2
 import grpc
 import time
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Benchmark:
     def __init__(
         self,
         program,
-        machine=os.environ.get("MACHINE", "machine"),
         target="backend.rustgym.com",
     ):
+        machine = os.environ.get("MACHINE")
+        if machine:
+            print(f"MACHINE is not empty and its value is: {machine}")
+        else:
+            raise ValueError(f"MACHINE is empty")
         channel = grpc.secure_channel(target, grpc.ssl_channel_credentials())
         self.stub = benchmark_pb2_grpc.BenchmarkServiceStub(channel)
         self.program = program
-        self.machine = machine
+        self.machine = os.environ["MACHINE"]
         self.start_time = int(time.time())
 
     def SetEpochTime(self, epoch_id):
